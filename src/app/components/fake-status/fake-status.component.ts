6922867bfcdb7523c4426f60e74167b872a95bf7
Torsten Kunkel <git@tkunkel.de>
2021-06-28 21:18:15 +0200

    * more random status

--- src/app/components/fake-status/fake-status.component.ts
+++ src/app/components/fake-status/fake-status.component.ts
@@ -1,6 +1,6 @@
-import { OnDestroy, Component, OnInit } from '@angular/core';
+import {OnDestroy, Component, OnInit} from '@angular/core';
 import * as _ from 'lodash';
-import { Subscription } from 'rxjs/internal/Subscription';
+import {Subscription} from 'rxjs/internal/Subscription';
 import {
   animate,
   keyframes,
@@ -17,10 +17,10 @@ import {
     trigger('updatedText', [
       transition('* => *', [
         //reset
-        animate('0s', keyframes([style({ transform: 'translateX(-10%)' })])),
-        animate('20s', keyframes([style({ transform: 'translateX(100%)' })])),
+        animate('0s', keyframes([style({transform: 'translateX(-10%)'})])),
+        animate('20s', keyframes([style({transform: 'translateX(100%)'})])),
         //pause
-        animate('30s', keyframes([style({ transform: 'translateX(100%)' })])),
+        animate('30s', keyframes([style({transform: 'translateX(100%)'})])),
       ]),
     ]),
   ],
@@ -39,12 +39,23 @@ export class FakeStatusComponent implements OnInit, OnDestroy {
     'Ordering pizza for next break',
     'Feeding coffee to coding monkeys',
     'Deploying new version announced tomorrow',
+    'Cloud performance might be reduced due to fast winds …',
+    'Some servers in the cloud are unavaiable due to heavy storms …',
+    'Removing caches to save storage',
+    'Splitting list to prepare for two parallel streams',
+    'The NADIN Modules lived together in harmony but everything changed when the Fleet Agency Business attacked',
+    'Never argue with the data',
+    'Debugging you must',
+    'Nothing shocks me – I fix bugs in NADIN',
+    'One NADIN to rule them all',
+    'Data cloud is full, please switch to local data storage solutions',
   ];
   private usedTexts: string[] = [];
   private tickerTimer: Subscription;
   public currentText: string = '';
 
-  constructor() {}
+  constructor() {
+  }
 
   public ngOnDestroy(): void {
     if (this.tickerTimer) {
