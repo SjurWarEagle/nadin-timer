1079a40d54fe25adbdf4e7883b95fb2673e137f5
Torsten Kunkel <git@tkunkel.de>
2021-06-28 14:34:27 +0200

    * add 8min step * update libs

--- src/app/components/fake-status/fake-status.component.ts
+++ src/app/components/fake-status/fake-status.component.ts
@@ -18,9 +18,9 @@ import {
       transition('* => *', [
         //reset
         animate('0s', keyframes([style({ transform: 'translateX(-10%)' })])),
-        animate('15s', keyframes([style({ transform: 'translateX(100%)' })])),
+        animate('20s', keyframes([style({ transform: 'translateX(100%)' })])),
         //pause
-        animate('25s', keyframes([style({ transform: 'translateX(100%)' })])),
+        animate('30s', keyframes([style({ transform: 'translateX(100%)' })])),
       ]),
     ]),
   ],
